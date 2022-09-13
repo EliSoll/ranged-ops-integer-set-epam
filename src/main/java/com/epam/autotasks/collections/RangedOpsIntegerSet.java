30ab3a086a12b60d70c3538d983bb9a347379a6e
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-09-13 20:51:40 +0300

    test 1

--- src/main/java/com/epam/autotasks/collections/RangedOpsIntegerSet.java
+++ src/main/java/com/epam/autotasks/collections/RangedOpsIntegerSet.java
@@ -1,35 +1,66 @@
 package com.epam.autotasks.collections;
 
-import java.util.AbstractSet;
-import java.util.Iterator;
+
+
+import java.util.*;
+
 
 class RangedOpsIntegerSet extends AbstractSet<Integer> {
+    private final TreeSet<Integer> set = new TreeSet<>();
+
 
     public boolean add(int fromInclusive, int toExclusive) {
-        throw new UnsupportedOperationException();
+
+        boolean flag = false;
+        for(int i = fromInclusive; i < toExclusive; i++){
+           if(!set.contains(i)) flag = true;
+           set.add(i);
+       }
+       return flag;
     }
 
     public boolean remove(int fromInclusive, int toExclusive) {
-        throw new UnsupportedOperationException();
+
+        int counter = 0;
+        for(int i = fromInclusive; i < toExclusive; i++){
+            if(set.contains(i)){
+                set.remove(i);
+                counter++;
+            }
+        }
+    return counter>0;
     }
+
 
     @Override
     public boolean add(final Integer integer) {
-        throw new UnsupportedOperationException();
+        boolean flag = !set.contains(integer);
+
+        set.add(integer);
+
+        return flag;
     }
 
     @Override
     public boolean remove(final Object o) {
-        throw new UnsupportedOperationException();
+        boolean flag = false;
+            if(set.contains(o)){
+                set.remove(o);
+                flag = true;
+            }
+        return flag;
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        throw new UnsupportedOperationException();
+        return set.iterator();
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return set.size();
     }
 }
+
+
+
